eef7fff213171acad51579848f78540f52c55c33
shath1955 <74083424+shath1955@users.noreply.github.com>
2025-04-03 15:27:39 -0400

    added slider gui to show age progression

--- face_morphing_GUI.m
+++ face_morphing_GUI.m
@@ -1,130 +1,136 @@
-function ageProgressionApp()
-    % Create main window
-    fig = uifigure('Name', 'Age Progression Morphing', 'Position', [100 100 800 600]);
+function sliderMorphApp()
+    % Create UI figure
+    fig = uifigure('Name', 'Age Morphing Slider', 'Position', [100 100 1000 600]);
 
     % Global variables
-    global img1 img2 points1 points2 saveDir
+    global img1 img2 points1 points2 morphedAxes img_size alpha tri p1 p2
     img1 = []; img2 = [];
     points1 = []; points2 = [];
-    saveDir = '';
+    img_size = [500 500];
+    alpha = 0.5;
 
-    % Panels
-    panelYoung = uipanel(fig, 'Title', 'Young Image', 'Position', [20 320 350 260]);
-    panelOld   = uipanel(fig, 'Title', 'Old Image',   'Position', [400 320 350 260]);
+    % Status labels
+    youngStatus = uilabel(fig, 'Text', 'Young image: Not loaded', ...
+        'Position', [30 490 300 20], 'FontColor', 'red');
 
-    % Axes
-    youngAxes = uiaxes(panelYoung, 'Position', [10 10 330 230]); youngAxes.XTick = []; youngAxes.YTick = [];
-    oldAxes   = uiaxes(panelOld,   'Position', [10 10 330 230]); oldAxes.XTick = []; oldAxes.YTick = [];
+    oldStatus = uilabel(fig, 'Text', 'Old image: Not loaded', ...
+        'Position', [200 490 300 20], 'FontColor', 'red');
 
-    % Buttons
+    % Upload buttons
     uibutton(fig, 'Text', 'Upload Young Image', ...
-        'Position', [30 250 150 30], 'BackgroundColor', '#0072BD', 'FontColor', 'w', ...
-        'ButtonPushedFcn', @(btn, event) uploadImage('young', youngAxes));
+        'Position', [30 520 150 30], ...
+        'BackgroundColor', '#0072BD', 'FontColor', 'w', ...
+        'ButtonPushedFcn', @(btn, event) uploadImage('young'));
 
     uibutton(fig, 'Text', 'Upload Old Image', ...
-        'Position', [200 250 150 30], 'BackgroundColor', '#0072BD', 'FontColor', 'w', ...
-        'ButtonPushedFcn', @(btn, event) uploadImage('old', oldAxes));
+        'Position', [200 520 150 30], ...
+        'BackgroundColor', '#0072BD', 'FontColor', 'w', ...
+        'ButtonPushedFcn', @(btn, event) uploadImage('old'));
 
-    uibutton(fig, 'Text', 'Select 17 Feature Points', ...
-        'Position', [30 200 320 30], 'BackgroundColor', '#EDB120', ...
+    % Select points
+    uibutton(fig, 'Text', 'Select Feature Points', ...
+        'Position', [30 450 320 30], ...
+        'BackgroundColor', '#EDB120', ...
         'ButtonPushedFcn', @(btn, event) selectPoints());
 
-    uibutton(fig, 'Text', 'Generate Age Progression GIF', ...
-        'Position', [30 150 320 30], 'BackgroundColor', '#77AC30', 'FontColor', 'w', ...
-        'ButtonPushedFcn', @(btn, event) runMorph());
+    % Slider
+    uilabel(fig, 'Text', 'Age Progression', 'Position', [30 400 120 20]);
+    slider = uislider(fig, ...
+        'Position', [30 380 300 3], ...
+        'Limits', [0 1], ...
+        'Value', alpha, ...
+        'MajorTicks', [0 0.5 1], ...
+        'ValueChangedFcn', @(sld, event) updateMorph(sld.Value), ...
+        'ValueChangingFcn', @(sld, event) updateMorph(event.Value));
 
-    %% Upload Image
-    function uploadImage(which, ax)
-        [file, path] = uigetfile({'*.png;*.jpg;*.jpeg'}, ['Select ', which, ' image']);
+    % Display axes
+    morphedAxes = uiaxes(fig, 'Position', [380 50 580 500]);
+    morphedAxes.XTick = []; morphedAxes.YTick = [];
+
+    % --- Upload Image Function ---
+    function uploadImage(which)
+        [file, path] = uigetfile({'*.jpg;*.jpeg;*.png'}, ['Select ', which, ' image']);
         if isequal(file, 0); return; end
         img = imread(fullfile(path, file));
-        if size(img, 3) == 1; img = cat(3, img, img, img); end
-        img = imresize(img, [500 500]);
-        if strcmp(which, 'young'); img1 = img; else; img2 = img; end
-        saveDir = path;
-        imshow(img, 'Parent', ax); drawnow;
+        if size(img,3) == 1; img = repmat(img, 1, 1, 3); end
+        img = imresize(img, img_size);
+        if strcmp(which, 'young')
+            img1 = img;
+            youngStatus.Text = 'Young image: Loaded ✔';
+            youngStatus.FontColor = 'green';
+        else
+            img2 = img;
+            oldStatus.Text = 'Old image: Loaded ✔';
+            oldStatus.FontColor = 'green';
+        end
     end
 
-    %% Feature Point Selection
+    % --- Select Points Function ---
     function selectPoints()
         if isempty(img1) || isempty(img2)
             uialert(fig, 'Upload both images first.', 'Missing Input');
             return;
         end
-        points1 = getPoints(img1, 'Young');
-        points2 = getPoints(img2, 'Old');
-        uialert(fig, 'Points selected.', 'Done');
-    end
 
-    %% Morphing + GIF generation
-    function runMorph()
-        if isempty(points1) || isempty(points2)
-            uialert(fig, 'Please select points first.', 'Missing Points');
-            return;
+        figure, imshow(img1), title('Select 17 points in Young image');
+        for i = 1:17
+            [x, y] = ginput(1);
+            points1(i,:) = [x, y];
+            hold on; plot(x, y, 'ro'); drawnow;
         end
+        close;
+
+        figure, imshow(img2), title('Select 17 points in Old image');
+        for i = 1:17
+            [x, y] = ginput(1);
+            points2(i,:) = [x, y];
+            hold on; plot(x, y, 'go'); drawnow;
+        end
+        close;
 
         boundary_pts = [0, 0; 250, 0; 500, 0; 0, 250; 500, 250; 0, 500; 250, 500; 500, 500];
         p1 = [points1; boundary_pts];
         p2 = [points2; boundary_pts];
         tri = delaunay(p1(:,1), p1(:,2));
-        alphas = linspace(0, 1, 30);
-        gif_filename = fullfile(saveDir, 'age_progression.gif');
 
-        waitbarHandle = waitbar(0, 'Generating GIF...');
-        for idx = 1:length(alphas)
-            alpha = alphas(idx);
-            p_morph = (1 - alpha) * p1 + alpha * p2;
-            morphed_img = uint8(zeros(size(img1)));
-
-            for i = 1:size(tri,1)
-                tri1 = p1(tri(i,:), :);
-                tri2 = p2(tri(i,:), :);
-                tri_morphed = p_morph(tri(i,:), :);
-                try
-                    T1 = fitgeotrans(tri1, tri_morphed, 'affine');
-                    T2 = fitgeotrans(tri2, tri_morphed, 'affine');
-                catch
-                    continue;
-                end
-                mask = poly2mask(tri_morphed(:,1), tri_morphed(:,2), 500, 500);
-                warp1 = imwarp(img1, T1, 'OutputView', imref2d([500 500]));
-                warp2 = imwarp(img2, T2, 'OutputView', imref2d([500 500]));
-                blended = (1 - alpha) * double(warp1) + alpha * double(warp2);
-                blended = uint8(blended);
-
-                for ch = 1:3
-                    region = morphed_img(:,:,ch);
-                    temp = blended(:,:,ch);
-                    region(mask) = temp(mask);
-                    morphed_img(:,:,ch) = region;
-                end
-            end
-
-            [A, map] = rgb2ind(morphed_img, 256);
-            if idx == 1
-                imwrite(A, map, gif_filename, 'gif', 'LoopCount', Inf, 'DelayTime', 0.1);
-            else
-                imwrite(A, map, gif_filename, 'gif', 'WriteMode', 'append', 'DelayTime', 0.1);
-            end
-            waitbar(idx / length(alphas), waitbarHandle);
-        end
-        close(waitbarHandle);
-
-        % Show in new figure
-        uialert(fig, 'GIF saved! Opening preview...', 'Complete');
-        web(gif_filename); % 
-
+        updateMorph(alpha);
     end
 
-    %% Point Picker
-    function pts = getPoints(img, label)
-        figure('Name', [label ' Image']), imshow(img), title(['Select 17 points in ', label]);
-        pts = zeros(17,2);
-        for i = 1:17
-            [x, y] = ginput(1);
-            pts(i,:) = [x, y];
-            hold on; plot(x, y, 'ro', 'MarkerSize', 5, 'MarkerFaceColor', 'r'); drawnow;
+    % --- Morphing Function ---
+    function updateMorph(a)
+        alpha = a;
+        if isempty(tri); return; end
+
+        morphed_points = (1 - alpha) * p1 + alpha * p2;
+        morphed_img = uint8(zeros([img_size 3]));
+
+        for i = 1:size(tri,1)
+            tri1 = p1(tri(i,:), :);
+            tri2 = p2(tri(i,:), :);
+            tri_morphed = morphed_points(tri(i,:), :);
+
+            try
+                T1 = fitgeotrans(tri1, tri_morphed, 'affine');
+                T2 = fitgeotrans(tri2, tri_morphed, 'affine');
+            catch
+                continue;
+            end
+
+            mask = poly2mask(tri_morphed(:,1), tri_morphed(:,2), img_size(1), img_size(2));
+            warp1 = imwarp(img1, T1, 'OutputView', imref2d(img_size));
+            warp2 = imwarp(img2, T2, 'OutputView', imref2d(img_size));
+            blended = (1 - alpha) * double(warp1) + alpha * double(warp2);
+            blended = uint8(blended);
+
+            for ch = 1:3
+                region = morphed_img(:,:,ch);
+                temp = blended(:,:,ch);
+                region(mask) = temp(mask);
+                morphed_img(:,:,ch) = region;
+            end
         end
-        close;
+
+        imshow(morphed_img, 'Parent', morphedAxes);
+        drawnow;
     end
 end
